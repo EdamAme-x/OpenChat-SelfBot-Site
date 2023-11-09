4e3371880b51b3601da62839bca4cb5b2c462d65
EdamAme-x <edame8080@gmail.com>
2023-11-09 12:28:59 +0900

    add design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,5 +4,30 @@ export default {
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		function({ addUtilities }) {
+			const newUtilities = {
+			  ".text-shadow": {
+				textShadow: "0px 2px 3px darkgrey"
+			  },
+			  ".text-shadow-md": {
+				textShadow: "0px 3px 3px darkgrey"
+			  },
+			  ".text-shadow-lg": {
+				textShadow: "0px 5px 3px darkgrey"
+			  },
+			  ".text-shadow-xl": {
+				textShadow: "0px 7px 3px darkgrey"
+			  },
+			  ".text-shadow-2xl": {
+				textShadow: "0px 10px 3px darkgrey"
+			  },
+			  ".text-shadow-none": {
+				textShadow: "none"
+			  }
+			};
+	  
+			addUtilities(newUtilities);
+		  }
+	],
 }
